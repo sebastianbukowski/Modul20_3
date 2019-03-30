08aeee53dd3b45468dc88369ba2f3189f8138673
SebastianBukowski <seb-27@o2.pl>
2019-03-30 22:37:46 +0100

    Redy task

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Router, Route, hashHistory } from 'react-router';
+// import { Provider } from 'react-redux';
+// import store from './store';
+// import Navigation from './presentational/navigation.component';
+
+// render(
+//     <Provider store={store}>
+//         <Router history={hashHistory}>
+//             <Route path='/' component={Navigation}>
+
+//             </Route>
+//         </Router>
+//     </Provider>,
+//     document.getElementById('root')
+// );
+
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-
-
-
+import routes from './routes';
 
 render(
     <Provider store={store}>
-    <div>
-   
-        <h1>Inicjalizacja projektu</h1>
-       <DevTools />
-    </div>    
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
