6244a126bf0687101f48763db962ac6826661e90
SebastianBukowski <seb-27@o2.pl>
2019-04-02 22:17:30 +0200

    Fix delete button bug

--- src/presentational/flag-list.component.js
+++ src/presentational/flag-list.component.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router';
 import CountryFlag from '../presentational/flag.component';
 
-const CountryFlagList = (props) =>(
+
+const CountryFlagList = (props) =>{
+    console.log(props)
+    return (
     <div className="countries-list">
         {props.countries.map(country => {
             return (
@@ -10,11 +13,11 @@ const CountryFlagList = (props) =>(
                     <Link className='logo' to={'countries/country/' + country.id}>
                         <CountryFlag country={country} />
                     </Link>
-                    <button onClick={props.deleteCountry.bind(null, country.id)}>DELETE</button>
+                    <button onClick={props.deleteCountry.bind(null,country.id)}>DELETE</button>
                 </div>
             )
         })}
     </div>
-);
+)};
 
 export default CountryFlagList;
